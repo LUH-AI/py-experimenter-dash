d6fee257f48c2c449b535215d2d7afa92f33a4ae
Andreas Bentzen Winje <a.winje@ai.uni-hannover.de>
2025-10-08 20:26:47 +0200

    rm old code

--- py_experimenter_dash/routes/dashboard.py
+++ py_experimenter_dash/routes/dashboard.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from py_experimenter_dash.db import get_experiment_counts, get_table
+from py_experimenter_dash.db import get_experiment_counts
 
 templates = Jinja2Templates(directory="py_experimenter_dash/templates")
 router = APIRouter()
@@ -21,20 +21,3 @@ async def counts_fragment(request: Request):
     """HTMX endpoint that returns only the updated counts."""
     counts = get_experiment_counts()
     return templates.TemplateResponse("partials/counts.html", {"request": request, "counts": counts})
-
-
-@router.get("/jobs", response_class=JSONResponse)
-async def get_jobs(request: Request):
-    """
-    Returns the table as a json.
-    """
-
-    table = get_table()
-    table = table.fillna(-1).to_dict()
-    table_keys = table.keys()
-    table_len = len(table[next(iter(table_keys))])
-    table = [{k: table[k][i] for k in table_keys} for i in range(table_len)]
-
-    table = sorted(table, key=lambda x: x["creation_date"], reverse=True)
-
-    return table
