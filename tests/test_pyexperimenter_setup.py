b5f3cd0dc869a97f3e4fa4fc358f956dc8aa4d96
mwever <m.wever@ai.uni-hannover.de>
2025-10-08 09:50:20 +0200

    Added test setup

---
+++ tests/test_pyexperimenter_setup.py
@@ -0,0 +1,34 @@
+import random
+from time import sleep
+
+from py_experimenter.experimenter import PyExperimenter
+
+produce_errors = True
+
+
+def run_experiment(config, result_processor, custom_config):
+    sleep(10 * random.random())
+    result_processor.process_results({
+        "accuracy": random.random(),
+        "traintime": random.randint(1, 5000),
+        "testtime": random.randint(1, 100),
+        "done": "True",
+    })
+    random.random()
+
+
+def run_error_experiment(config, result_processor, custom_config):
+    if random.random() > 0.25:
+        raise ValueError("foo")
+    else:
+        raise NotImplementedError()
+
+
+pyexp = PyExperimenter(
+    experiment_configuration_file_path="experiment_config.yml",
+    database_credential_file_path="database_connection.yml",
+    use_codecarbon=False,
+)
+pyexp.fill_table_from_config()
+pyexp.execute(run_experiment, max_experiments=10)
+pyexp.execute(run_error_experiment, max_experiments=10)
