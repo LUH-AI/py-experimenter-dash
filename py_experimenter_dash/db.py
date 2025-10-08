ad75cba5717ba8cf419fb19faf196fc832cf1fc6
mwever <m.wever@ai.uni-hannover.de>
2025-10-08 10:54:37 +0200

    Added some dynamic behavior for the counts.

--- py_experimenter_dash/db.py
+++ py_experimenter_dash/db.py
@@ -1,9 +1,15 @@
+created = 90
+
+
 def get_experiment_counts():
     """Return counts of experiments by status."""
+    global created
+    if created > 0:
+        created = created - 1
     return {
         "total": 100,
-        "open": 90,
+        "created": created,
         "running": 1,
-        "done": 4,
+        "done": 4 + (100 - created - 9),
         "error": 5,
     }
