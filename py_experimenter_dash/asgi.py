a6b9b31384645710eb11db485f4d295f11daf789
mwever <m.wever@ai.uni-hannover.de>
2025-10-08 12:24:51 +0200

    Added joblist page.

--- py_experimenter_dash/asgi.py
+++ py_experimenter_dash/asgi.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from py_experimenter_dash.routes import carbon_footprint, dashboard, error_page, query
+from py_experimenter_dash.routes import carbon_footprint, dashboard, error_page, joblist, query
 
 templates = Jinja2Templates(directory="py_experimenter_dash/templates")
 
@@ -31,3 +31,4 @@ app.include_router(dashboard.router)
 app.include_router(error_page.router)
 app.include_router(query.router)
 app.include_router(carbon_footprint.router)
+app.include_router(joblist.router)
